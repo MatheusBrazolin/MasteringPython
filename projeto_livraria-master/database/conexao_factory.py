7edadf5539675968fecd7b147864697b623bae81
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2024-08-27 19:14:58 -0300

    Delete projeto_livraria-master directory

--- projeto_livraria-master/database/conexao_factory.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-
-class ConexaoFactory:
-    def get_conexao(self):
-        return psycopg2.connect(host="silly.db.elephantsql.com", database="dmhesvhi", user="dmhesvhi", password="zbeWx5cZCPZCEL0ZwVC815YyYzb27kjo")
