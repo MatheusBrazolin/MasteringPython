20e38d9f6adf58ae2105809bc6d5c7d5c07cbeaf
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2024-08-27 19:11:39 -0300

    Delete Projeto Livraria directory

--- Projeto Livraria/main.py
+++
@@ -1,8 +0,0 @@
-from service.categoria_service import CategoriaService
-from service.autor_service import AutorService
-
-service = CategoriaService()
-service.menu()
-
-service = AutorService()
-service.menu()
